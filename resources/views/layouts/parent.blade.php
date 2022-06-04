fe2e299da895c6f461f0394dd390ef8784d1fdb7
Michael Reinhard <michael.reinhard@student.arteveldehs.be>
2022-06-04 19:31:26 +0200

    Add favicon

--- resources/views/layouts/parent.blade.php
+++ resources/views/layouts/parent.blade.php
@@ -7,6 +7,7 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        {{-- icons --}}
         <link rel="apple-touch-icon" sizes="180x180" href={{asset('icons/apple-touch-icon.png')}}>
         <link rel="icon" type="image/png" sizes="32x32" href={{asset('icons/favicon-32x32.png')}}>
         <link rel="icon" type="image/png" sizes="16x16" href={{asset('icons/favicon-16x16.png')}}>
